cf1bd8a01e9db41add72cf0a352022d0f490489c
Davitron <segunmatthews@outlook.com>
2019-04-16 13:06:58 +0100

    feat(vpc network): add instances to public subnets
    - add haproxy instance
    - add NAT instance
    
    [#165371780]

--- network/variables.tf
+++ network/variables.tf
@@ -5,10 +5,37 @@ variable "private_subnet_cidr" {
   default = ""
 }
 
+variable "master_ip" {
+  default = ""
+}
+
+
+variable "slave1_ip" {
+  default = ""
+}
+
+variable "slave2_ip" {
+  default = ""
+}
+
+variable "image" {
+  default = ""
+}
+
 variable  "region" {
   default = ""
 }
 
 variable "project" {
   default = ""
+}
+
+
+
+variable "startup_scripts" {
+  type = "map"
+  default = {
+    nat = "sudo sysctl -w net.ipv4.ip_forward=1; sudo iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE"
+    haproxy = "cd /home/packer; sudo su - packer; sudo chmod +x haproxy.sh; bash haproxy.sh"
+  }
 }
